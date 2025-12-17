433304e5d9bb780939f47b477a5d7aac7b0ab207
GellyBird <zombiecupcake0409@gmail.com>
2025-12-17 18:32:08 -0500

    More to solution and bugfixing

--- solution.java
+++ solution.java
@@ -3,30 +3,57 @@ import java.util.Arrays;
 
 public class solution {
     // The point must be the closest to the goal (move towards it) & The safest (lowest risk value)
-
-
     public static int[][] mappedGrid = inputGenerator.getTestInput();
+    // int[][] mappedGrid = inputGenerator.createInput();
 
     public static void main(String[] args) {
-        // int[][] mappedGrid = inputGenerator.createInput();
-        System.out.println(Arrays.deepToString(mappedGrid).replace("], ","]\n"));
-        int[] coordinatePt = new int[6]; // A list of the calculated risk vals around a point
+        String[] coordinatePt = new String[6]; // A list of the calculated risk vals around a point
         int[] riskVals = new int[6]; // The coordinates that corespond with each risk val. Easier to handle if I just do 2 seperate lists.
+        int[] distVals = new int[6]; // The distance away of each coordinate point
         int currentCoordinateIdx = 0; // Where you are in the lists
         int firstCoord = 1; // X val
         int secondCoord = 1; // Y val
+        String coordinates = ""; // All of the traveled to points
+
+        System.out.println(Arrays.deepToString(mappedGrid).replace("], ","]\n"));
 
         while (firstCoord != 10 & secondCoord != 10) { // While the "elf" is not at the "factory" (1,1 to 10,10)
             for (int i = -1; i < 2; i ++) { // Check surrounding points
                 for (int j = -1; j < 2; j ++) {
-                    if (i != 0 | j != 0 && mappedGrid[firstCoord + i][secondCoord + j] != 0) { // Make sure it's not an edge case ("0") or the current point (i.e [1,1])
-                        riskVals[currentCoordinateIdx] = checkRisk(firstCoord + i, secondCoord + j);
-                        coordinatePt[currentCoordinateIdx] = mappedGrid[firstCoord + i][secondCoord + j];
-                        currentCoordinateIdx++;
+                    if ((i != 0 | j != 0)) { // Make sure it's not an edge case ("0") or the current point (i.e [1,1])
+                        if (firstCoord + i > 0 && secondCoord + j > 0) {
+                            riskVals[currentCoordinateIdx] = checkRisk(firstCoord + i, secondCoord + j);
+                            coordinatePt[currentCoordinateIdx] = i+","+j;
+                            currentCoordinateIdx++;
+                        }
                     }
                 }
-            } 
+            }
+
+            if (currentCoordinateIdx != 5) { // Just fill remaining values with placeholders if there's nothing there.
+                for (int i = currentCoordinateIdx ; i < 6 ; i++) {
+                    coordinatePt[i] = "";
+                    riskVals[i] = 10000;
+                }
+            }
+            
+            currentCoordinateIdx = 0;
+            for (int i = 0; i < coordinatePt.length; i ++) {
+                if (riskVals[i] < riskVals[currentCoordinateIdx]) {
+                    currentCoordinateIdx = i;
+                }
+            }
+
+
+            firstCoord = Integer.parseInt(coordinatePt[currentCoordinateIdx].substring(0,1));
+            secondCoord = Integer.parseInt(coordinatePt[currentCoordinateIdx].substring(2));
+            coordinates += firstCoord + secondCoord;
+            currentCoordinateIdx = 0;
+
+            System.out.println("("+firstCoord+","+secondCoord+")");
         }
+
+        System.out.println(coordinates);
     }
             
 
@@ -35,7 +62,7 @@ public class solution {
 
         for (int idx1 = -1; idx1 < 2; idx1 ++) { // Check surrounding points
             for (int idx2 = -1; idx2 < 2; idx2 ++) {
-                if ((idx1 != 0 | idx2 != 0) && mappedGrid[i][j] != 0) {
+                if (idx1 != 0 | idx2 != 0) {
                     totalRisk += mappedGrid[i + idx1][j + idx2];
                 }
             }
