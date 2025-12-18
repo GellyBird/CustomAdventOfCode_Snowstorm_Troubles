b9cb0cd5f0b0b69fc0230b63155f32f23e339e2a
GellyBird <zombiecupcake0409@gmail.com>
2025-12-17 21:17:05 -0500

    finished I hope

--- solution.java
+++ solution.java
@@ -1,4 +1,6 @@
 //https://stackoverflow.com/questions/19648240/the-best-way-to-print-a-java-2d-array
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class solution {
@@ -7,73 +9,82 @@ public class solution {
     // int[][] mappedGrid = inputGenerator.createInput();
 
     public static void main(String[] args) {
-        String[] coordinatePt = new String[6]; // A list of the calculated risk vals around a point
-        int[] riskVals = new int[6]; // The coordinates that corespond with each risk val. Easier to handle if I just do 2 seperate lists.
-        int[] distVals = new int[6]; // The distance away of each coordinate point
+
+        ArrayList<String> coordinatePt = new ArrayList<>(); // The coordinates that corespond with each risk val
+        ArrayList<Integer> riskVals = new ArrayList<>(); // A list of the calculated risk vals around a point 
+        ArrayList<Double> distVals = new ArrayList<>(); // The distance from the end point for each coordinate
+
         int currentCoordinateIdx = 0; // Where you are in the lists
         int firstCoord = 1; // X val
         int secondCoord = 1; // Y val
-        String coordinates = ""; // All of the traveled to points
+        String coordinates = "11"; // All of the traveled to points
 
-        System.out.println(Arrays.deepToString(mappedGrid).replace("], ","]\n"));
-
-        while (firstCoord != 10 & secondCoord != 10) { // While the "elf" is not at the "factory" (1,1 to 10,10)
+        while (firstCoord < 10 | secondCoord < 10) { // While the "elf" is not at the "factory" (1,1 to 10,10)
             for (int i = -1; i < 2; i ++) { // Check surrounding points
                 for (int j = -1; j < 2; j ++) {
-                    if ((i != 0 | j != 0)) { // Make sure it's not an edge case ("0") or the current point (i.e [1,1])
-                        if (firstCoord + i > 0 && secondCoord + j > 0) {
-                            riskVals[currentCoordinateIdx] = checkRisk(firstCoord + i, secondCoord + j);
-                            coordinatePt[currentCoordinateIdx] = i+","+j;
-                            currentCoordinateIdx++;
+                    if (i != 0 | j != 0) { // Make sure it's not the current point (i.e [1,1])
+                        if ((i != -1 | j != -1) && (i != -1 | j != 1) && (i != 1 | j != -1) && (i != 1 | j != 1) && getDistance(firstCoord, secondCoord) > getDistance(firstCoord + i, secondCoord + j)) {
+                            if ((firstCoord + i) > 0 && (secondCoord + j) > 0 & (firstCoord + i) < 11 && (secondCoord + j) < 11) {
+                                riskVals.add(checkRisk(firstCoord + i, secondCoord + j));
+                                distVals.add(getDistance(firstCoord + i, secondCoord + j));
+                                coordinatePt.add((firstCoord + i)+","+(secondCoord + j));
+                                currentCoordinateIdx++;
+                            }
+                        }
+                    }
+                }
+            }
+            
+            currentCoordinateIdx = 0;
+            for (int i = 0; i < coordinatePt.size(); i ++) {
+                if (distVals.get(i) < getDistance(firstCoord, secondCoord)) {
+                    if (riskVals.get(i) < riskVals.get(currentCoordinateIdx) && distVals.get(i) < distVals.get(currentCoordinateIdx)) {
+                        currentCoordinateIdx = i;
+                    } else if ((riskVals.get(i) == riskVals.get(currentCoordinateIdx)) | (distVals.get(i) == distVals.get(currentCoordinateIdx)) ) {
+                        int iFirst = Integer.parseInt(coordinatePt.get(i).substring(0,coordinatePt.get(i).indexOf(",")));
+                        int iSecond = Integer.parseInt(coordinatePt.get(i).substring(coordinatePt.get(i).indexOf(",")+1));
+
+                        int currentFirst = Integer.parseInt(coordinatePt.get(currentCoordinateIdx).substring(0,coordinatePt.get(currentCoordinateIdx).indexOf(",")));
+                        int currentSecond = Integer.parseInt(coordinatePt.get(currentCoordinateIdx).substring(coordinatePt.get(currentCoordinateIdx).indexOf(",")+1));
+
+                        double riskINew = checkRisk(iFirst + 1, iSecond) + checkRisk(currentFirst, currentSecond + 1);
+                        double riskCurrentNew = checkRisk(currentFirst + 1, currentSecond) + checkRisk(currentFirst, currentSecond + 1);
+                        if (riskINew < riskCurrentNew) {
+                            currentCoordinateIdx = i;
                         }
                     }
                 }
             }
 
-            if (currentCoordinateIdx != 5) { // Just fill remaining values with placeholders if there's nothing there.
-                for (int i = currentCoordinateIdx ; i < 6 ; i++) {
-                    coordinatePt[i] = "";
-                    riskVals[i] = 10000;
-                }
-            }
-            
+            firstCoord = Integer.parseInt(coordinatePt.get(currentCoordinateIdx).substring(0,coordinatePt.get(currentCoordinateIdx).indexOf(",")));
+            secondCoord = Integer.parseInt(coordinatePt.get(currentCoordinateIdx).substring(coordinatePt.get(currentCoordinateIdx).indexOf(",")+1));
+            coordinates += firstCoord + "" + secondCoord;
+            mappedGrid[firstCoord][secondCoord] = 0;
+
             currentCoordinateIdx = 0;
-            for (int i = 0; i < coordinatePt.length; i ++) {
-                if (riskVals[i] < riskVals[currentCoordinateIdx]) {
-                    currentCoordinateIdx = i;
-                }
-            }
-
-
-            firstCoord = Integer.parseInt(coordinatePt[currentCoordinateIdx].substring(0,1));
-            secondCoord = Integer.parseInt(coordinatePt[currentCoordinateIdx].substring(2));
-            coordinates += firstCoord + secondCoord;
-            currentCoordinateIdx = 0;
-
-            System.out.println("("+firstCoord+","+secondCoord+")");
+            coordinatePt = new ArrayList<>();
+            riskVals = new ArrayList<>();
+            distVals = new ArrayList<>();
         }
 
+        
+        System.out.println(Arrays.deepToString(mappedGrid).replace("], ","]\n"));
         System.out.println(coordinates);
     }
             
 
     public static int checkRisk(int i, int j) { // Calculates the risk value of the point and the surrounding area. Helps prevents a 'Greedy' search.
-        int totalRisk = mappedGrid[i][j];
-
-        for (int idx1 = -1; idx1 < 2; idx1 ++) { // Check surrounding points
-            for (int idx2 = -1; idx2 < 2; idx2 ++) {
-                if (idx1 != 0 | idx2 != 0) {
-                    totalRisk += mappedGrid[i + idx1][j + idx2];
-                }
-            }
-        } 
-
-        return totalRisk;
+        int totalRisk = 10000;
+        if (i != 0 && j != 0 && i != 11 && j != 11) {
+            totalRisk = mappedGrid[i][j];
+            totalRisk += mappedGrid[i+1][j];
+            totalRisk += mappedGrid[i][j+1];
+        }
+            return totalRisk;
     }
 
     public static double getDistance(int i, int j) { // Find distance between point and end goal
         double distance = Math.sqrt(Math.pow((10 - i), 2) + Math.pow((10 - j), 2));
-        System.out.println(distance);
         return distance;
     }
 }
